06b3433689e3e86d3568bef61126016d7733dc1d
e-inquirer <sorour.a@gmail.com>
2017-03-20 17:00:47 -0400

    added `cheese` template direcotry for index; update `CheeseController.CheeseController()` return.

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -14,6 +14,6 @@ public class CheeseController {
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index() {
-        return "index";
+        return "cheese/index";
     }
 }
