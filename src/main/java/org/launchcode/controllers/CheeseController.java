bdbfe237d253ba9ab44c120ab6f6df2be42933f5
e-inquirer <sorour.a@gmail.com>
2017-03-20 16:56:01 -0400

    added base request mapping `/cheese` to `CheeseController.CheeseController()`

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by ahmed on 3/20/17.
  */
-
-//comment stub
-
 @Controller
+@RequestMapping("cheese")
 public class CheeseController {
 
+    // Request path: /cheese
     @RequestMapping(value = "")
     public String index() {
         return "index";
